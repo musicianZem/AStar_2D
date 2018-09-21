94280abaf2a5cc8a15be5ba42100df8d2a66a7d2
musicianZem <201224510@pusan.ac.kr>
2018-09-21 14:24:57 +0900

    vector version. NOT GOOD YET

--- main.cpp
+++ main.cpp
@@ -1,108 +1,242 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <set>
+#include <SDL2/SDL.h>
+#include <algorithm>
 using namespace std;
 
-int    map  [100][100];
-bool visited[100][100];
+int obstacle[160][90];
+int fromI = 0, fromJ = 0, goalI = 15, goalJ = 8;
+bool hasPath = true;
 const int I = 0;
 const int J = 1;
 const int D = 2;
-
 int nextPosition[8][3] = {
-    {-1, -1, 14},
-    {-1,  1, 14},
-    { 1, -1, 14},
-    { 1,  1, 14},
-    {-1,  0, 10},
-    { 1,  0, 10},
-    { 0, -1, 10},
-    { 0,  1, 10}
+    {-1, -1, 3}, {-1,  1, 3}, { 1, -1, 3}, { 1,  1, 3},
+    {-1,  0, 2}, { 1,  0, 2}, { 0, -1, 2}, { 0,  1, 2}
 };
+
+bool isOutofBound(int i, int j) {
+    return i < 0 || i >= 160 || j < 0 || j >= 90;
+}
 
 class Cell {
     public :
         int i, j;
-        int move;
-        int need;
-        int parent_i, parent_j;
+        int FScore, GScore, HScore;
+        // F : total
+        // G : 이 Cell까지 걸린 비용
+        // H : 예상 비용
+        int pi, pj;
 
-        Cell(int i=0, int j=0) : i(i), j(j) {
+        Cell(int i = 0, int j = 0) : i(i), j(j) {}
+
+        void calHScore() {
+            HScore = 2 * (abs(goalI - i) + abs(goalJ - j));
         }
 
-        void getNeedManhattan( int goal_i, int goal_j ) {
-            need = abs(goal_i - i) + abs(goal_j - j);
-            need *= 10;
-        }
-
-        friend bool operator < ( const Cell &c1, const Cell &c2 ) {
-            return c1.move + c1.need > c2.move + c2.need;
+        void calFScore() {
+            FScore = HScore + GScore;
         }
 };
 
+bool compCell( const Cell& c1, const Cell& c2 ) {
+    return c1.FScore > c2.FScore;
+}
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+vector<pair<int, int>> path;
+
+void initSDL();
+bool pollEvent();
+void render();
+void astar();
+
 int main() {
-    
-    // get my position
-    int my_i = 10;
-    int my_j = 82;
+    initSDL();
 
-    // get goal position
-    int goal_i = 77;
-    int goal_j = 15;
+    while( !pollEvent() ) {
+        SDL_RenderClear( renderer );
 
-    const int ARRIVED_DISTANCE = 15;
+        astar();
+        render();
 
-    priority_queue< Cell >  openPQ;
-    vector< Cell > closeVector;
-    
-    Cell tempCell(my_i, my_j);
-    tempCell.parent_i = my_i; tempCell.parent_j = my_j;
-    tempCell.getNeedManhattan( goal_i, goal_j );
-    tempCell.move = 0;
-    closeVector.push_back( tempCell );
-
-    for(int i=0; i<8; i++) {
-        Cell direct8( tempCell.i + nextPosition[i][I], tempCell.j + nextPosition[i][J] );
-        direct8.parent_i = tempCell.i; direct8.parent_j = tempCell.j;
-        direct8.getNeedManhattan( goal_i, goal_j );
-        direct8.move = tempCell.move + nextPosition[i][D];
-        openPQ.push( direct8 );
+        SDL_RenderPresent( renderer );
     }
+    SDL_DestroyWindow( window );
+    SDL_DestroyRenderer( renderer );
+    SDL_Quit();
+}
 
-    while( !openPQ.empty() ) {
-        Cell c = openPQ.top(); openPQ.pop();
-        if( c.i < 0 || c.j < 0 || c.i >= 100 || c.j >= 100 ) continue;
-        if( visited[ c.i ][ c.j ] ) continue;
-        visited[ c.i ][ c.j ] = true;
-        closeVector.push_back(c);
-        if( c.need < ARRIVED_DISTANCE ) {
-            break;
-        }
+void astar() {
+    path.clear();
+    vector<Cell> O, C;
+    Cell firstCell( fromI, fromJ );
+    firstCell.GScore = 0;
+    firstCell.FScore = firstCell.HScore;
+    firstCell.calHScore();
+    firstCell.pi = fromI;
+    firstCell.pj = fromJ;
+    O.push_back( firstCell );
+    obstacle[1][3] = 1;
+    obstacle[2][3] = 1;
+    obstacle[3][3] = 1;
+    obstacle[3][2] = 1;
+    obstacle[3][1] = 1;
 
-        for(int i=0; i<8; i++) {
-            Cell direct8( c.i + nextPosition[i][I], c.j + nextPosition[i][J] );
-            direct8.parent_i = c.i; direct8.parent_j = c.j;
-            direct8.move = c.move + nextPosition[i][D];
-            direct8.getNeedManhattan( goal_i, goal_j );
-            openPQ.push( direct8 );
-        }
-    }
+    while( !O.empty() ) {
+        sort( O.begin(), O.end(), [](const Cell& o1, const Cell& o2){ return o1.FScore < o2.FScore; });
+        Cell beforeCell = O[0];
+        C.push_back( beforeCell );
+        O.erase( O.begin() );
 
-    vector<Cell> path;
-    for(vector<Cell>::reverse_iterator rit = closeVector.rbegin(); rit != closeVector.rend(); rit++) {
-        Cell c = *rit;
-        if( path.empty() ) {
-            path.push_back( c );
-        } else {
-            if( path.back().parent_i == c.i && path.back().parent_j == c.j ) {
-                path.push_back( c );
+        if( beforeCell.HScore < 1 ) break;
+
+        Cell nextCell;
+        for(int pos=0; pos<8; pos++) {
+            int i = beforeCell.i + nextPosition[pos][0];
+            int j = beforeCell.j + nextPosition[pos][1];
+
+            if( !isOutofBound(i, j) ) {
+                if( obstacle[i][j] != 0 ) continue;
+                bool isExist = false;
+                for(auto &c : C) {
+                    if( c.i == i && c.j == j ) {
+                        isExist = true;
+                        break;
+                    }
+                }
+                if( isExist ) continue;
+                nextCell.i = i; nextCell.j = j;
+                nextCell.GScore = beforeCell.GScore + nextPosition[pos][2];
+                nextCell.calHScore();
+                nextCell.calFScore();
+                nextCell.pi = beforeCell.i;
+                nextCell.pj = beforeCell.j;
+
+                for(auto &o : O) {
+                    if( o.i == i && o.j == j ) {
+                        isExist = true;
+                        if( o.FScore > nextCell.FScore ) {
+                            o = nextCell;
+                        }
+                        break;
+                    }
+                }
+                if( !isExist ) {
+                    O.push_back( nextCell );
+                }
             }
         }
     }
 
-    cout << "from : " <<  my_i  << ", " <<  my_j  << endl;
-    cout << " to  : " << goal_i << ", " << goal_j << endl;
-    for(auto &p : path)  {
-        cout << p.i << " " << p.j << endl; 
+    int traceI = C.back().pi, traceJ = C.back().pj;
+    while( traceI != fromI || traceJ != fromJ ) {
+        obstacle[ traceI ][ traceJ ] = -1;
+        for(vector<Cell>::iterator it = C.begin(); it != C.end(); ) {
+            if( it->i == traceI && it->j == traceJ ) {
+                traceI = it->pi;
+                traceJ = it->pj;
+                C.erase( it );
+                break;
+            } else {
+                it++;
+            }
+        }
     }
+    cout << traceI << " " << fromI << " " << traceJ << " " << fromJ << endl;
+    if( traceI == fromI && traceJ == fromJ ) {
+        hasPath = true;
+    } else {
+        hasPath = false;
+    }
+}
+
+void initSDL() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    window = SDL_CreateWindow("ASTAR", 0, 0, 1600, 900, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+}
+
+bool pollEvent() {
+    SDL_Event e;
+    bool quit = false;
+    static bool setStart = true;
+    static int mouse_sx, mouse_sy, mouse_ex, mouse_ey;
+    while( SDL_PollEvent( &e ) != 0 ) {
+        switch( e.type ) {
+            case SDL_QUIT :
+                quit = true;
+                break;
+            case SDL_MOUSEBUTTONDOWN :
+                SDL_GetMouseState( &mouse_sx, &mouse_sy );
+                if( e.button.button == SDL_BUTTON_LEFT ) {
+                }
+                if( e.button.button == SDL_BUTTON_RIGHT ) {
+                    if( setStart ) {
+                        fromI = mouse_sx / 12;
+                        fromJ = mouse_sy / 12;
+                    } else {
+                        goalI = mouse_sx / 12;
+                        goalJ = mouse_sy / 12;
+                    }
+                    setStart = setStart == false;
+                }
+                break;
+            case SDL_MOUSEBUTTONUP :
+                SDL_GetMouseState( &mouse_ex, &mouse_ey );
+                if( e.button.button == SDL_BUTTON_LEFT ) {
+                    if( mouse_sx > mouse_ex ) swap( mouse_sx, mouse_ex );
+                    if( mouse_sy > mouse_ey ) swap( mouse_sy, mouse_ey );
+                    int si = mouse_sx/12, sj = mouse_sy/12;
+                    int ei = mouse_ex/12, ej = mouse_ey/12;
+                    for(int i=si; i<=ei; i++) {
+                        for(int j=sj; j<=ej; j++) {
+                            obstacle[ i ][ j ] = 1;
+                        }
+                    }
+                }
+                if( e.button.button == SDL_BUTTON_RIGHT ) {
+                }
+                break;
+        }
+    }
+    return quit;
+}
+
+void render() {
+    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 0 );
+    SDL_Rect rect;
+    rect.w = 12; rect.h = 12;
+    for(int i=0; i<160; i++) {
+        for(int j=0; j<90; j++) {
+            rect.x = i*12;
+            rect.y = j*12;
+            if( i == goalI && j == goalJ ) {
+                SDL_SetRenderDrawColor( renderer, 0, 0, 255, 0 );
+                SDL_RenderFillRect( renderer, &rect );
+                continue;
+            }
+            if( i == fromI && j == fromJ ) {
+                SDL_SetRenderDrawColor( renderer, 0, 255, 0, 0 );
+                SDL_RenderFillRect( renderer, &rect );
+                continue;
+            }
+            if( obstacle[i][j] == 1 ) {
+                SDL_SetRenderDrawColor( renderer, 100, 100, 100, 100 );
+                SDL_RenderFillRect( renderer, &rect );
+                continue;
+            }
+            if( obstacle[i][j] == -1  && hasPath ) {
+                printf("%3d, %3d -> %3d, %3d path of one, [%3d][%3d]\n",fromI, fromJ, goalI, goalJ, i, j);
+                SDL_SetRenderDrawColor( renderer, 255, 0, 0, 0 );
+                SDL_RenderFillRect( renderer, &rect );
+                obstacle[i][j] = 0;
+                continue;
+            }
+        }
+    }
+    SDL_SetRenderDrawColor( renderer, 255, 255, 255, 0 );
 }
